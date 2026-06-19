14af6830d2fe756bb4acbec0b7ac572009ff48a3
Oliver Bestmann <oliver.bestmann@googlemail.com>
2026-06-19 22:53:28 +0200

    update gnome patches

--- default.nix
+++ default.nix
@@ -60,10 +60,8 @@
                 ./patches/mutter/0004-backends-native-Fix-cursor-visibility-check-off-by-one-50.2.patch
               ];
               "50.3" = [
+                # i've collapsed it to a single patch
                 ./patches/mutter/0001-backends-native-Move-cursor-to-overlay-plane-50.3.patch
-                ./patches/mutter/0002-backend-native-Use-less-padding-on-a-single-monitor-50.3.patch
-                ./patches/mutter/0003-backends-native-Pass-cursor-padding-to-cursor-manage-50.3.patch
-                ./patches/mutter/0004-backends-native-Fix-cursor-visibility-check-off-by-one-50.3.patch
               ];
             };
           in
